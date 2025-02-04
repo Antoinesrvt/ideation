b446a75f8fadefd1169d095c576b22086dc62719
Antoinesrvt <antoine.srvt@gmail.com>
2025-02-04 15:49:06 +0100

    removed old code

--- src/app/offers/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Offers from '../components/offers'
-
-const page = () => {
-  return <Offers />;
-};
-
-export default page;
--- src/app/your-business/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import YourBusiness from '../components/your-business'
-
-const page = () => {
-  return <YourBusiness />;
-};
-
-export default page
